249bb53eb8774a3439b20f055b3ffa433517377f
advem12 <bondar00798gmail.com>
2020-01-17 20:26:06 +0200

    fix css

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Увійти &lsaquo; velomag1 &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/https://adven12.github.io/bicycles/wp-includes/css/dashicons.min.css?ver=5.3.2' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/https://adven12.github.io/bicycles/wp-includes/css/buttons.min.css?ver=5.3.2' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/https://adven12.github.io/bicycles/wp-admin/css/forms.min.css?ver=5.3.2' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/https://adven12.github.io/bicycles/wp-admin/css/l10n.min.css?ver=5.3.2' media='all' />
-<link rel='stylesheet' id='login-css'  href='/https://adven12.github.io/bicycles/wp-admin/css/login.min.css?ver=5.3.2' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-includes/css/dashicons.min.css?ver=5.3.2' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-includes/css/buttons.min.css?ver=5.3.2' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-admin/css/forms.min.css?ver=5.3.2' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-admin/css/l10n.min.css?ver=5.3.2' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-admin/css/login.min.css?ver=5.3.2' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -25,7 +25,7 @@
 		<div id="login">
 		<h1><a href="https://uk.wordpress.org/">Працює на WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="/https://adven12.github.io/bicycles/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Ім'я користувача або Email адреса</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
@@ -49,34 +49,34 @@
 		</form>
 
 					<p id="nav">
-									<a href="/https://adven12.github.io/bicycles/wp-login.php?action=lostpassword">Втратили свій пароль?</a>
+									<a href="https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-login.php?action=lostpassword">Втратили свій пароль?</a>
 								</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
-				<p id="backtoblog"><a href="/https://adven12.github.io/bicycles/">
+				<p id="backtoblog"><a href="https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/">
 		&larr; Назад до velomag1		</a></p>
 			</div>
-	<script src='/https://adven12.github.io/bicycles/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
-<script src='/https://adven12.github.io/bicycles/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+	<script src='https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script src='https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
 <script>
 var _zxcvbnSettings = {"src":"\/wp-includes\/js\/zxcvbn.min.js"};
 </script>
-<script src='/https://adven12.github.io/bicycles/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
+<script src='https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
 <script>
 var pwsL10n = {"unknown":"\u0421\u0438\u043b\u0430 \u043f\u0430\u0440\u043e\u043b\u044e \u043d\u0435\u0432\u0456\u0434\u043e\u043c\u0430","short":"\u0414\u0443\u0436\u0435 \u0441\u043b\u0430\u0431\u043a\u0438\u0439","bad":"\u0421\u043b\u0430\u0431\u043a\u0438\u0439","good":"\u0421\u0435\u0440\u0435\u0434\u043d\u0456\u0439","strong":"\u0421\u0438\u043b\u044c\u043d\u0438\u0439","mismatch":"\u041f\u0430\u0440\u043e\u043b\u0456 \u043d\u0435 \u0441\u043f\u0456\u0432\u043f\u0430\u0434\u0430\u044e\u0442\u044c"};
 </script>
-<script src='/https://adven12.github.io/bicycles/wp-admin/js/password-strength-meter.min.js?ver=5.3.2'></script>
-<script src='/https://adven12.github.io/bicycles/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
+<script src='https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-admin/js/password-strength-meter.min.js?ver=5.3.2'></script>
+<script src='https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
 <script>
 var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 </script>
-<script src='/https://adven12.github.io/bicycles/wp-includes/js/wp-util.min.js?ver=5.3.2'></script>
+<script src='https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-includes/js/wp-util.min.js?ver=5.3.2'></script>
 <script>
 var userProfileL10n = {"warn":"\u0412\u0430\u0448 \u043d\u043e\u0432\u0438\u0439 \u043f\u0430\u0440\u043e\u043b\u044c \u043d\u0435 \u0437\u0431\u0435\u0440\u0435\u0436\u0435\u043d\u043e.","warnWeak":"\u0414\u043e\u0437\u0432\u043e\u043b\u0438\u0442\u0438 \u0432\u0438\u043a\u043e\u0440\u0438\u0441\u0442\u0430\u043d\u043d\u044f \u0441\u043b\u0430\u0431\u043a\u043e\u0433\u043e \u043f\u0430\u0440\u043e\u043b\u044e.","show":"\u041f\u043e\u043a\u0430\u0437\u0430\u0442\u0438","hide":"\u0421\u0445\u043e\u0432\u0430\u0442\u0438","cancel":"\u0421\u043a\u0430\u0441\u0443\u0432\u0430\u0442\u0438","ariaShow":"\u041f\u043e\u043a\u0430\u0437\u0430\u0442\u0438 \u043f\u0430\u0440\u043e\u043b\u044c","ariaHide":"\u0421\u0445\u043e\u0432\u0430\u0442\u0438 \u043f\u0430\u0440\u043e\u043b\u044c"};
 </script>
-<script src='/https://adven12.github.io/bicycles/wp-admin/js/user-profile.min.js?ver=5.3.2'></script>
+<script src='https://adven12.github.io/bicycles/https:/adven12.github.io/https://adven12.github.io/bicycles/wp-admin/js/user-profile.min.js?ver=5.3.2'></script>
 	<script>
 	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
 	</script>
